a6afead15e989969608ef420e2bca27bc2026896
Checo Gonzales <chc.gnzls@gmail.com>
2017-03-11 15:27:18 -0500

    added R to reformat csv data

---
+++ format_mob_data.R
@@ -0,0 +1,9 @@
+#!/bin/R
+
+df <- read.csv("nbhds_online_data_table4.csv", stringsAsFactor=FALSE)
+st <- as.character(df$state_id)
+st[which(nchar(st) == 1)] <- paste0("0", st[which(nchar(st) == 1)])
+cty <- do.call(rbind, strsplit(as.character(unique(df$cty2000/1000)+0.0001), "\\."))[,2]
+cty <- gsub("1$", "", cty)
+df$GEOID <- paste0(st, cty)
+write.csv(df, file="./csv/nbhds.csv", row.names=FALSE)
