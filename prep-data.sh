b96ccda34f269d871018f5f078d9d6d7ffddce75
montesboi <chc.gnzls@gmail.com>
2017-03-11 15:29:52 -0500

    added .gitignore

--- prep-data.sh
+++ prep-data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #  Downlaod
-curl -O "www2.census.gov/geo/tiger/GENZ2014/shp/ch_2014_us_county_500k.zip" 
+curl "www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_us_county_500k.zip" -o ./shp/cb_2014_us_county_500k.zip 
+unzip ./shp/cb_2014_us_county_500k.zip -d ./shp/
 
 #  Turn .shp to .json
 shp2json ./shp/cb_2014_us_county_500k.shp -o ./json/usa.json
@@ -29,6 +30,6 @@ ndjson-map -r d3 'd.properties.fill = d3.scaleSequential(d3.iterpolateViridis).d
 
 #  Compress
 geo2topo -n cty=./json/usa-cty_p25_kr26.ndjson > usa-cty-topo.json
-toposimplify -p 1 -f < usa-cty-topo.json > sim.json
+toposimplify -p 1 -f < usa-cty-topo.json > usa-sm.json
 topoquantize 1e5 < sim.json > sim-q.json
 
