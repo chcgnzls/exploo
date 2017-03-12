b5eb5c2eb2fcd85f24dda4841b9d52b8675f2398
montesboi <chc.gnzls@gmail.com>
2017-03-12 14:14:05 -0400

    lets add tooltip now!

--- prep-data.sh
+++ prep-data.sh
@@ -1,32 +1,32 @@
 #!/bin/bash -x
 
 #  Downlaod
-curl "www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_us_county_500k.zip" -o ./shp/cb_2014_us_county_500k.zip 
-unzip -o ./shp/cb_2014_us_county_500k.zip -d ./shp/
+#curl "www2.census.gov/geo/tiger/GENZ2014/shp/cb_2014_us_county_500k.zip" -o ./shp/cb_2014_us_county_500k.zip 
+#unzip -o ./shp/cb_2014_us_county_500k.zip -d ./shp/
 
 #  Turn .shp to .json
-shp2json ./shp/cb_2014_us_county_500k.shp -o ./json/usa.json
+#shp2json ./shp/cb_2014_us_county_500k.shp -o ./json/usa.json
 
 #  Project to Albers
-geoproject 'd3.geoAlbersUsa()' < ./json/usa.json > ./json/usa-albers.json
-ndjson-split 'd.features' < ./json/usa-albers.json > ./json/usa-albers.ndjson
-ndjson-map 'd.GEOID = d.properties.GEOID, d' < ./json/usa-albers.ndjson > ./json/usa-albers-id.ndjson
+#geoproject 'd3.geoAlbersUsa()' < ./json/usa.json > ./json/usa-albers.json
+#ndjson-split 'd.features' < ./json/usa-albers.json > ./json/usa-albers.ndjson
+#ndjson-map 'd.GEOID = d.properties.GEOID, d' < ./json/usa-albers.ndjson > ./json/usa-albers-id.ndjson
 
 #  Prepare Mobility Data
-Rscript "format_mob_data.R"
-csv2json ./csv/nbhds.csv ./json/nbhds.json
-ndjson-cat ./json/nbhds.json | ndjson-split 'd.slice(1)' > ./json/nbhds.ndjson
+#Rscript "format_mob_data.R"
+#csv2json ./csv/nbhds.csv ./json/nbhds.json
+#ndjson-cat ./json/nbhds.json | ndjson-split 'd.slice(1)' > ./json/nbhds.ndjson
 
 #  Merge Geodata and Mob data
-ndjson-join 'd.GEOID' ./json/usa-albers-id.ndjson ./json/nbhds.ndjson > ./json/usa-nbhds.ndjson
+#ndjson-join 'd.GEOID' ./json/usa-albers-id.ndjson ./json/nbhds.ndjson > ./json/usa-nbhds.ndjson
 
 #  Use d3 to fill causal_p25_cty_kr26 color
-ndjson-map 'd[0].properties = {outcome: d[1].causal_p25_cty_kr26}, d[0]' < ./json/usa-nbhds.ndjson > ./json/usa-cty_p25_kr26.ndjson
+ndjson-map 'd[0].properties = {outcome: d[1].causal_p25_cty_kr26, county: d[0]["properties"]["NAME"]}, d[0]' < ./json/usa-nbhds.ndjson > ./json/usa-cty_p25_kr26.ndjson
 ndjson-map 'd[0].properties = {outcome: d[1].causal_p25_cz_cty_kr26}, d[0]' < ./json/usa-nbhds.ndjson > ./json/usa-cz_p25_kr26.ndjson
 
 MAPTHIS="./json/usa-cty_p25_kr26.ndjson"
 
-ndjson-map -r d3 '(d.properties.fill = d3.scaleLinear(d3.interpolateViridis).domain(d3.range(d3.min(d.properties.outcome),d3.max(d.properties.outcome))) (d.properties.outcome), d)' < "$MAPTHIS" > usa-color.ndjson
+ndjson-map -r d3 '(d.properties.fill = d3.scaleLinear(d3.interpolateViridis).domain([-5,5]) (d.properties.outcome), d)' < "$MAPTHIS" > usa-color.ndjson
 
 #  Compress
 geo2topo -n cty="$MAPTHIS" > usa-cty-topo.json
