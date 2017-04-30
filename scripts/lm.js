a8fc309b4d854a98fb1e365dbb1de417fdee5d90
Checo Gonzales <chc.gnzls@gmail.com>
2017-04-30 11:59:06 -0400

    drops missing

--- scripts/lm.js
+++ scripts/lm.js
@@ -1,2 +1,13 @@
-var y = mobData.map(function(d){return d[lhsVar];});
-var X = mobData.map(function(d){return [d[rhsVars[0]], d[rhsVars[1]]];});
+var ym = mobData.map(function(d){return Number(d[lhsVar]);});
+var Xm = mobData.map(function(d){return [d[rhsVars[0]], d[rhsVars[1]]].map(Number);});
+
+var X = Xm.filter(function(d, i){
+		var k = [ym[i]];
+		k = k.concat(d);
+		return !numeric.any(numeric.isNaN(k));
+	});
+var y = ym.filter(function(d, i){
+		var k = [d];
+		k = k.concat(Xm[i]);
+		return !numeric.any(numeric.isNaN(k));
+	});
