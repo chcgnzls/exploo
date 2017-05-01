4a67b11adb0803ec8cce9331714e7d12c0709ffd
Checo Gonzales <chc.gnzls@gmail.com>
2017-05-01 11:01:18 -0400

    ...

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,5 @@
+var nm = numeric;
+
 var mapThis = "perm_res_p25_kr26"
 var projScale = 1;
 var width = 960 * projScale, 
@@ -23,8 +25,8 @@ var keys;
 var outcomeKeys;
 var yourData;
 var mobData = [];
-
 var rhsVars = [];
+var result;
 
 //  Number format functions
 var fd = d3.format(".2f");
@@ -200,7 +202,35 @@ function loadElements(yourData) {
 		if (rhsVars.length < 1) {
 			alert("no rhs variables were selected...");
 		} else {
-			lhsVar = selectors[3][selectors[3].selectedIndex].text
+			var lhsVar = selectors[3][selectors[3].selectedIndex].text
+			var ym = mobData.map(function(d){return Number(d[lhsVar]);});
+			var Xm = mobData.map(function(d){return rhsVars.map(function(e){
+				return Number(d[e]);});});
+			var X = Xm.filter(function(d, i){
+				var k = [ym[i]];
+				k = k.concat(d);
+				return !numeric.any(numeric.isNaN(k));
+			});
+			X = X.map(function(d){return [1].concat(d);});
+			var y = ym.filter(function(d, i){
+				var k = [d];
+				k = k.concat(Xm[i]);
+				return !numeric.any(numeric.isNaN(k));
+			});
+	
+			var meanDep = nm.transpose(X).map(function(d){
+				return d.reduce(function(acc, val){return acc + val;}, 0) / d.length;});
+			var varDep = nm.transpose(X).map(function(d, i){
+				return d.map(function(a){
+					return Math.pow(a - meanDep[i], 2);})
+						.reduce(function(acc, val){return acc + val;}, 0) / d.length;
+			});
+
+			var XtXinv = nm.inv(nm.dot(nm.transpose(X), X));
+			var betas = nm.dot(XtXinv, nm.dot(nm.transpose(X), y)); 
+			var se = nm.getDiag(XtXinv).map(function(d, i){
+				return Math.sqrt(d * varDep[i]);});
+			result = {beta : betas, meanDep : meanDep, stdErr: se};
 		}
 	});
 
